3edac4caf0b4efba97e5c1e85279cd1455240c6d
unknown <joseph@jc-lab.net>
2025-03-04 00:38:17 +0900

    fix: publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     `java-library`
     kotlin("jvm") version Version.KOTLIN
+    id("com.vanniktech.maven.publish") version Version.VANNIKTECH_MAVEN_PUBLISH apply false
 }
 
 repositories {
     mavenCentral()
 }
+
+group = "io.mosaicboot"
+version = Version.PROJECT
 
 subprojects {
     group = "io.mosaicboot"
